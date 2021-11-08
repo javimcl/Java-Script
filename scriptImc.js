f02014d7ddd23c807729a2926223af1823a2e969
javimcl <javimcl1986@gmail.com>
2021-11-08 18:11:59 -0500

    calculo imc

---
+++ scriptImc.js
@@ -0,0 +1,20 @@
+window.onload = iniciar;
+
+function iniciar(){
+    var btnCalcular = document.getElementById("btnCalcular");
+    btnCalcular.addEventListener("click", clickBtnCalcular);
+
+}
+
+function clickBtnCalcular(){
+    var txtPeso = document.getElementById("txtPeso");
+    var peso = txtPeso.value;
+    
+    var txtAltura = document.getElementById("txtAltura");
+    var altura = txtAltura.value;
+    
+
+    var imc = peso / (altura * altura);
+    alert(imc);
+    
+}
