f45e75ea0743b2f0ea6b99e8fa95669bec88ae5b
javimcl <javimcl1986@gmail.com>
2021-11-08 16:45:16 -0500

    se sube todo a git

---
+++ script.js
@@ -0,0 +1,7 @@
+var nombre = prompt("Ingrese el nombre:");
+var edad = prompt("Ingrese su edad:");
+var telefono = prompt("Ingrese su telefono");
+
+var mensajeFinal = "Nombre: " + nombre + ". Edad: " +edad +" . Tel: " + telefono;
+
+alert(mensajeFinal);
