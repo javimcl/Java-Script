8279a05e2b0836eb9dd2bd2ccd7f4f9e0588aef3
javimcl <javimcl1986@gmail.com>
2021-11-10 15:40:28 -0500

    Create dom.js

---
+++ dom.js
@@ -0,0 +1,10 @@
+document.getElementsByTagName("h1");
+document.getElementById("txtNombre");
+//busqueda por clase
+document.querySelectorAll(".menu");
+//busqueda por id
+document.querySelectorAll("#btnAgregar");
+//busqueda por tag
+document.querySelectorAll("div");
+//Modificar text
+document.getElementById("header").innerHTML = "Titulo Ejmplo"
